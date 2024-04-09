f6fff2e52b2a37a467810c639a34bc88acb18c28
Stephan Strittmatter <stritti@users.noreply.github.com>
2024-04-09 18:43:05 +0000

    glow

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <ProgressOverlay v-if="showLoadingSpinner" :enabled="showLoadingSpinner" />
   <div v-else>
     <div v-for="(dateGroup, date) in groupedList" :key="date">
-      <h2>
+      <h2 class="mx-3">
         {{
           new Intl.DateTimeFormat('de-DE', {
             dateStyle: 'short'
@@ -10,7 +10,7 @@
         }}
       </h2>
       <div v-for="(timeGroup, time) in dateGroup" :key="time">
-        <h3>{{ time }} Uhr</h3>
+        <h3 class="mx-3">{{ time }} Uhr</h3>
         <agenda-item
           v-for="agendaItem in timeGroup"
           :key="agendaItem.date.getTime()"
