bf3adffbc4b105e2bf1b0459785b00c5f28afe7b
Stephan Strittmatter <stritti@users.noreply.github.com>
2024-04-10 06:05:33 +0000

    feat: PWA icons updated

--- vite.config.ts
+++ vite.config.ts
@@ -15,14 +15,30 @@ export default defineConfig({
       resolvers: [PrimeVueResolver()]
     }),
     VitePWA({
+      devOptions: {
+        enabled: true
+      },
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: [
+        'favicon.svg',
+        'robots.txt',
+        'apple-touch-icon-180x180.png',
+        'pwa-64x64.png',
+        'pwa-192x192.png',
+        'pwa-512x512.png',
+        'favicon.ico'
+      ],
       manifest: {
         name: 'SyKOM24',
         short_name: 'SyKOM24',
         description: 'SyKOM24',
-        theme_color: '#ffffff',
+        theme_color: '#18181b',
         icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
@@ -34,10 +50,10 @@ export default defineConfig({
             type: 'image/png'
           },
           {
-            src: 'apple-touch-icon.png',
+            src: 'maskable-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           }
         ]
       },
@@ -51,7 +67,7 @@ export default defineConfig({
               cacheName: 'api-cache',
               expiration: {
                 maxEntries: 100,
-                maxAgeSeconds: 2 * 60 * 60 // 2 Stunden
+                maxAgeSeconds: 0.5 * 60 * 60 // 0.5 Stunden
               },
               cacheableResponse: {
                 statuses: [0, 200]
