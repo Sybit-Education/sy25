e18dedd75421045d7735dcf888b22111f5e4411c
Stephan Strittmatter <stritti@users.noreply.github.com>
2024-04-11 08:53:38 +0000

    chore: PWA insets

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,17 +1,21 @@
 <template>
   <nav class="header-bar text-center">
-    <img class="logo m-1" src="/img/sykom24.png" alt="logo" />
+    <RouterLink to="/">
+      <img class="logo m-1" src="/img/sykom24.png" alt="logo" />
+    </RouterLink>
   </nav>
 </template>
 <style lang="scss">
 .header-bar {
   display: block;
-  height: 2.5rem;
   width: 100vw;
   margin: 0;
   position: fixed;
   top: 0px;
   left: 0px;
+  padding-top: calc(0.25rem + env(safe-area-inset-top));
+  padding-left: calc(env(safe-area-inset-left));
+  padding-right: calc(env(safe-area-inset-right));
   background: #18181b;
   z-index: 1000;
   border-bottom: #909090 1px solid;
