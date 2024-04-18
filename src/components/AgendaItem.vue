9621a3c59d402043d6f4050f9c3389d43deff85f
Stephan Strittmatter <stritti@users.noreply.github.com>
2024-04-18 23:56:50 +0200

    AgendaItem.vue aktualisieren

--- src/components/AgendaItem.vue
+++ src/components/AgendaItem.vue
@@ -2,7 +2,7 @@
   <div>
     <a v-if="agenda.isActive" name="active-agenda" ref="active-agenda" />
     <router-link :to="`/talk/${agenda.talk}/${agenda.id}`">
-      <Card class="agenda-item m-1" :class="statusClass" :style="`min-height: ${agenda.duration/15}px`">
+      <Card class="agenda-item m-1" :class="statusClass" :style="`min-height: ${agenda.duration ? agenda.duration/15 : 450/15}px`">
         <template #title>
           <div class="flex flex-col">
             <TalkItem :talkId="agenda.talk[0]" class="flex-1"></TalkItem>
